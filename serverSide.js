251e229e55276fd60dc88d8199144de2136dfb7c
univ <f.lewis20.class18@outlook.com>
2023-03-05 22:36:44 +0000

    serverSide.js

--- serverSide.js
+++ serverSide.js
@@ -1,21 +1,16 @@
-function all(func, args) {
-  var arr = func.split(".");
-
-  var libName = arr[0];
-  var libFunc = arr[1];
-
-  args = args || [];
-
-  return this[libName][libFunc].apply(this, args);
-}
-
 function doGet(e) {
-  var arr = ["foo", "doGet"] || e.parameter["func"].split(".");
-
+  var foobarr = e.parameter["func"] || "foo.doGet";
+  var arr = [foobarr].toString().split(".");
   var libName = arr[0];
   var libFunc = arr[1];
-
-  args = ["e"] || e.parameter["args"];
-
-  return this[libName][libFunc].apply(this, args);
+  args = [e.parameter["args"]] || [Utilities.jsonStringify(e)];
+  return foo.renderTemplate(this[libName][libFunc].apply(this, args));
 }
+
+var runAll = function (func, args) {
+  var arr = func.split(".");
+  var libName = arr[0];
+  var libFunc = arr[1];
+  args = args || [];
+  return this[libName][libFunc].apply(this, args);
+};
