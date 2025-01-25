6ec252cdfc33cfd55572d03752c689daedfda76d
univ <f.lewis20.class18@outlook.com>
2025-01-25 19:24:02 +0000

    .

--- RunAll.js
+++ RunAll.js
@@ -1,48 +1,157 @@
 var doGet = function (e) {
-  var foobarr = e.parameter["func"] || "renderFile";
   var libName = "foo";
-  var libFunc = foobarr;
-  var rndPage = [
-    `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS`,
-  ]
-    .toString()
-    .split(" ")[
-    Math.floor(
-      Math.random() *
-        Math.floor(
-          [
-            `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS`,
-          ]
-            .toString()
-            .split(" ").length,
-        ),
-    )
-  ];
-  var args = e.parameter["args"] || "jFundamentals";
-  return renderTemplate(
-    `
-      <div id="pageObj"></div>
-      <script> 
-        document.addEventListener("DOMContentLoaded", eRun)
-      function eRun() {
-        document.getElementById("pageObj").innerHTML = <?!= JSON.stringify(e) ?>} 
-      </script>
-      <?!= renBlob ?>
-      `,
-    {
-      renBlob: this[libName].contentApp(`<?!= appL ?>`, {
-        appL: this[libName][foobarr].apply(this, [args]),
-      }),
-
-      e: JSON.stringify(e),
-    },
+  e
+    ? e
+    : (e = this[libName].objectOfS(
+        ["parameter"],
+        [[["func", arguments.callee.name]]],
+        Math.floor((this[libName].maxTime - (new Date() % (1000 * 60))) / 1000),
+      ));
+  console.log(
+    Math.floor((this[libName].maxTime - (new Date() % (1000 * 60))) / 1000) +
+      "\n" +
+      arguments.callee.name +
+      "\n!" +
+      e.parameter["func"] +
+      ", = " +
+      !e.parameter["func"],
   );
+  var titleArray = [];
+  for (var key in globalThis) {
+    if (typeof globalThis[key] == "function") {
+      titleArray.push(key);
+    }
+  }
+  var objMaster = {
+    miscellaneous: {
+      section: titleArray,
+    },
+  };
+  var fx = e.parameter["func"];
+  if (fx === objMaster.miscellaneous.section[0]) {
+    return this[libName].handleRequest(e);
+  } else if (fx === objMaster.miscellaneous.section[5]) {
+    return this[libName].handleRequest(e);
+  } else {
+    var foobarr = "renderFile";
+    var htmlArray = [
+      `index proMedia epaWebsite callBack oddChances jsGame checkOnDay uiAccess popUpOpen congressLeg congressMembers jFundamentals gnuFree myGNUFreeJS Section3.Challenge1 cors edgarFriendly editor ssForms styling theRoll theWorks uiAccess`,
+    ]
+      .toString()
+      .split(" ");
+    var args =
+      htmlArray[Math.floor(Math.random() * Math.floor(htmlArray.length))];
+    if (this[libName].renderFile(args)) {
+      return renderTemplate(
+        `<html id="JavaScriptDoGet">
+          <head>
+            <base target="_self">
+            <meta charset="utf-8">
+            <meta name="Subscribe" content="ATL Budget Studio">
+            <meta name=viewport content="width=device-width, initial-scale=1">
+            <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+            <style>
+
+                body {
+
+                  flex-grow: 1;
+                  color:blue;
+                  text-decoration:bold;
+                  flex-flow: row wrap;
+                  grid-column: 1;
+                  grid-row: 1;
+                  text-align: center;
+                  align-content: flex-start;
+                  overflow: auto;
+                }
+            </style>
+          </head>
+          <body>
+            <div id="pageObj"></div>
+            <div>
+              <?!= renBlob ?>
+            </div>
+            <script> 
+              document.addEventListener("DOMContentLoaded", eRun)
+            function eRun() {
+              document.getElementById("pageObj").innerHTML = <?= JSON.stringify(e) ?>} 
+            </script>
+        `,
+        {
+          renBlob: this[libName].contentApp(
+            `
+              <html id="JavaScript">
+                <head>
+                  <base target="_self">
+                  <meta charset="utf-8">
+                  <meta name="Subscribe" content="JavaScript webapp">
+                  <meta name=viewport content="width=device-width, initial-scale=1">
+                  <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+                  <style>
+
+                      body {
+
+                        flex-grow: 1;
+                        color:blue;
+                        text-decoration:bold;
+                        flex-flow: row wrap;
+                        grid-column: 1;
+                        grid-row: 1;
+                        text-align: center;
+                        align-content: flex-start;
+                        overflow: auto;
+                      }
+                  </style>
+                </head>
+                <body>
+                  <div id="zeroSize">
+                    <?!= HtmlService.createTemplate(appL).evaluate().getContent() ?>
+                  </div> 
+                  <div class="row">
+                    <div class="col s8 l8 m8 card-panel push-m2 push-s2 push-l2">
+                      <div class="video-container"> 
+                          <iframe 
+                            src=""
+                            id="indexBeta"
+                            width="100%"
+                            height="100%"
+                            allow="autoplay"
+                            allow="encrypted-media"
+                            title="Dontime Life Website"
+                            frameborder="0"
+                            allowfullscreen
+                            ></iframe>
+                      </div>
+                    </div>
+                  </div>
+                  <script>
+                    console.log(<?!= appL.length ?>)
+                    if (<?!= appL.length === 99 || appL.length === 94 || appL.length === 83 ?>) {
+                      document.getElementById("zeroSize").innerHTML = ""
+                      document.getElementById("indexBeta").src = <?= appL ?>}
+                    else {document.getElementById("indexBeta").src = "https://www.clubhouse.com/@fabianlewis?utm_medium=ch_profile&utm_campaign=lhTUtHb2bYqPN3w8EEB7FQ-247242"}
+                  </script>
+                </body>
+              </html>
+                `,
+            {
+              appL: this[libName][foobarr].apply(this, [args]),
+            },
+          ),
+          e: e,
+        },
+        args,
+      );
+    } else {
+      return this[libName].handleRequest(e);
+    }
+  }
 };
 
-var runBoilerplate = function (func, args) {
+var runBoilerplate = function (func, someArgs) {
   var libName = "foo";
   var libFunc = func || "doGet";
-  args = args || [];
+  args = someArgs || [];
   return this[libName][libFunc].apply(this, args);
 };
 
