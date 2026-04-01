db449f8bf979239ff6938eed95d42da250c05780
Living Water <f.lewis20.livingwater19@outlook.com>
2026-04-01 20:22:32 +0000

    app.js

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ function onYouTubeIframeAPIReady() {
   });
   function onPlayerReady(event) {
     event.target.loadPlaylist("UU6DOFpA9UCTgNwJiVX1IOpQ", ctr);
-    setTimeout(setShuffle);
+    setShuffle();
+    setTimeout(nextvideo);
   }
 
   // 5. The API calls this function when the player's state changes.
