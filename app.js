0b7b283d79d15de3840b9e9adb1b12dbacd9dbf0
Living Water <f.lewis20.livingwater19@outlook.com>
2026-04-02 13:40:26 +0000

    app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ function onYouTubeIframeAPIReady() {
     } else if (event.data == YT.PlayerState.BUFFERING) {
       changeBorderColor(event.data);
       setShuffle();
-      setTimeout(playVideo);
+      event.target.playVideo();
       event.target.neztVideo();
     } else if (event.data == YT.PlayerState.VIDEO_CUED) {
       changeBorderColor(event.data);
