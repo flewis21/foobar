18f7ae6ef11ce3c32d91756a81823a68f1ba9795
Living Water <f.lewis20.livingwater19@outlook.com>
2026-06-01 03:27:46 +0000

    app.js

--- app.js
+++ app.js
@@ -276,14 +276,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // }
     // console.log(event);
   }
+
+  function cancelRequest(event) {
+    hError.textContent = "Error fetching data:";
+  }
 });
 
 function reFetch(event) {
   hError.textContent = "Loading...";
-}
-
-function cancelRequest(event) {
-  hError.textContent = "Error fetching data:";
 }
 
 function move() {
